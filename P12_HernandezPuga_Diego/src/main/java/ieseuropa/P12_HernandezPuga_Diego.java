8f2c465b487af53367a9aa133b047a24c1b3de72
dev <dev@debian>
2023-03-24 20:52:59 +0100

    Metodos comprobacion etapa y curso

--- P12_HernandezPuga_Diego/src/main/java/ieseuropa/P12_HernandezPuga_Diego.java
+++ P12_HernandezPuga_Diego/src/main/java/ieseuropa/P12_HernandezPuga_Diego.java
@@ -1,9 +1,45 @@
 package main.java.ieseuropa;
 
+import java.util.ArrayList;
+
 public class P12_HernandezPuga_Diego {
+	
+	private static ArrayList<String> listaEtapas(){
+		ArrayList<String> etapas = new ArrayList<>();
+		etapas.add("Secundaria");
+		etapas.add("Bachillerato");
+		etapas.add("SMR");
+		etapas.add("DAW");
+		etapas.add("DAM");
+		return etapas;
+	}
+	
+	private static boolean esEtapaCorrecta(String etapa) {
+		if(listaEtapas().contains(etapa))
+			return true;
+		else
+			return false;
+	}
+	
+	private static boolean esCursoCorrecto(String etapa, int curso) {
+		if(etapa.equals("Secundaria")) {
+			if(curso >= 1 && curso <= 4) {
+				return true;
+			}else {
+				return false;
+			}
+		}else {
+			if(curso == 1 || curso == 2) {
+				return true;
+			}else {
+				return false;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<Profesor> profesores = new ArrayList<>();
 
 	}
 
